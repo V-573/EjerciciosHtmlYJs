a9c5628589b0a35dea0e01109ab028820af54de3
vaj573 <jhonedw573@gmail.com>
2024-01-11 22:41:45 -0500

    queda funcionando menu mobile y menu desktop y aside trabajando en congruencia por medio de js

--- main.js
+++ main.js
@@ -2,16 +2,55 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+    console.log("undido email")
+     const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!desktopMenu.classList.contains('inactive') && !isAsideClosed ) {
+     
+        aside.classList.add('inactive');
+     
+    } else {
+         desktopMenu.classList.toggle('inactive');
+        
+}
+
+       
+    
     
 }
-function  toggleMobilMenu() {
+
+
+function toggleMobilMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+    
+}
+
+function toggleCarritoAside() {
+     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!desktopMenu.classList.contains('inactive')) {
+     desktopMenu.classList.add('inactive')
+ }
+
+
+    aside.classList.toggle('inactive');
     
 }
 
